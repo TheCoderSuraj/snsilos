3879d998ab8d7e5633a29d97abc2b0ef8c47c411
thecodersuraj <surajthecoder@gmail.com>
2023-06-27 20:20:21 +0545

    firebase configuration

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,20 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:sn_silos/core/general/screens/home_screen.dart';
 import 'package:sn_silos/utils/theme_data.dart';
 
+import 'firebase_options.dart';
 import 'utils/route_generator.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // initializing firebase
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  // running application
   runApp(const SnSilosApp());
 }
 
